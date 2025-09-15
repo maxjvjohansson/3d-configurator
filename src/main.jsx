60b0ad8f5ffc61058212e44355df9318053e8cfb
maxjvjohansson <max.jv.johansson@gmail.com>
2025-09-15 12:47:36 +0200

    Add: Base Navbar component along with basic styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./globals.css";
 import App from "./App.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Navbar />
     <App />
   </StrictMode>
 );
