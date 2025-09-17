80fdd23d087d19c0c2c751f865bbbf62befd0c31
anna-dahlberg <anna.dahlberg@live.se>
2025-09-17 17:36:21 +0200

    Fix: Rendering issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import Navbar from "./components/Navbar/Navbar.jsx";
-import ConfiguratorProvider from "./context/ConfiguratorContext.jsx";
+import Navbar from './components/Navbar/Navbar.jsx'
+import Configurator from './components/Configurator/Configurator.jsx'
+import ConfiguratorProvider from './context/ConfiguratorContext.jsx'
 
 function App() {
-  return (
-    <ConfiguratorProvider>
-      <header>
-        <Navbar />
-      </header>
-      <main></main>
-    </ConfiguratorProvider>
-  );
+	return (
+		<ConfiguratorProvider>
+			<header>
+				<Navbar />
+			</header>
+			<main>
+				<Configurator />
+			</main>
+		</ConfiguratorProvider>
+	)
 }
 
-export default App;
+export default App
