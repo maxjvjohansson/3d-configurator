8433aad488e810cd6de61cf4fd790be055863381
maxjvjohansson <max.jv.johansson@gmail.com>
2025-09-17 11:02:29 +0200

    Update: Navbar elements and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,16 +3,22 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <a href="/">3D Configurator</a>
+      <a href="/" className="logo">
+        <span className="logo-light">BUY</span>
+        <span className="logo-bold">BUD</span>
+      </a>
       <ul className="nav-list">
         <li>
-          <a href="/">Link 1</a>
+          <a href="/">Store</a>
         </li>
         <li>
-          <a href="/">Link 2</a>
+          <a href="/">Support</a>
         </li>
         <li>
-          <a href="/">Link 3</a>
+          <a href="/">FAQ</a>
+        </li>
+        <li>
+          <button className="cart-btn">ADD TO CART</button>
         </li>
       </ul>
     </nav>
