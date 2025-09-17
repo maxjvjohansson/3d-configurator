d912bce53f6859faac4dbe15c0f90e4bdbb1a5aa
anna-dahlberg <anna.dahlberg@live.se>
2025-09-17 15:17:34 +0200

    Add: Functionality to render canvas w/ mock file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Navbar from "./components/Navbar/Navbar.jsx";
+import Navbar from './components/Navbar/Navbar.jsx'
+import Configurator from './components/Configurator/Configurator.jsx'
 
 function App() {
-  return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-      <main></main>
-    </>
-  );
+	return (
+		<>
+			<header>
+				<Navbar />
+			</header>
+			<main>
+				<Configurator />
+			</main>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/Configurator/Configurator.jsx
+++ src/components/Configurator/Configurator.jsx
@@ -1,26 +1,18 @@
 import './Configurator.css'
-import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
-import {
-	OrbitControls, // Camera controls
-	Environment, // Easy lighting/HDRI
-	ContactShadows, // Ground shadows
-	PresentationControls, // Alternative to OrbitControls
-	Stage, // Quick scene setup
-	Center, // Center objects
-	Text, // 3D text
-	Html, // HTML overlays in 3D space
-	useGLTF, // Load GLTF/GLB models
-	useTexture, // Load textures
-	Bounds, // Auto-fit camera to objects
-	AccumulativeShadows, // Realistic shadows
-	RandomizedLight // Multiple light setup
-} from '@react-three/drei'
-import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+
+function CreditCard() {
+	const { scene } = useGLTF('/models/credit_card/scene.gltf')
+	return <primitive object={scene} />
+}
 
 export default function Configurator() {
 	return (
 		<section>
-			<div></div>
+			<Canvas>
+				<CreditCard />
+			</Canvas>
 		</section>
 	)
 }
