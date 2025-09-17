fb63b3e47df58985bf16a17d64f130ef5fd14019
maxjvjohansson <max.jv.johansson@gmail.com>
2025-09-17 13:06:48 +0200

    Imported ContextProvider to App.jsx and the use of state variables in Configurator.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
+import ConfiguratorProvider from "./context/ConfiguratorContext.jsx";
 
 function App() {
   return (
-    <>
+    <ConfiguratorProvider>
       <header>
         <Navbar />
       </header>
       <main></main>
-    </>
+    </ConfiguratorProvider>
   );
 }
 
