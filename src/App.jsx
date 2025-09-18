f5a5b9aa7ac6c25fbf20028709bab7f27a559ccd
maxjvjohansson <max.jv.johansson@gmail.com>
2025-09-18 17:40:10 +0200

    Add: TextInput component along with base styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import Navbar from './components/Navbar/Navbar.jsx'
-import Configurator from './components/Configurator/Configurator.jsx'
-import ConfiguratorProvider from './context/ConfiguratorContext.jsx'
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Configurator from "./components/Configurator/Configurator.jsx";
+import ConfiguratorProvider from "./context/ConfiguratorContext.jsx";
+import TextInput from "./components/TextInput/TextInput.jsx";
 
 function App() {
-	return (
-		<ConfiguratorProvider>
-			<header>
-				<Navbar />
-			</header>
-			<main>
-				<Configurator />
-			</main>
-		</ConfiguratorProvider>
-	)
+  return (
+    <ConfiguratorProvider>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Configurator />
+        <TextInput label="Text" placeholder="Your text here..." />
+      </main>
+    </ConfiguratorProvider>
+  );
 }
 
-export default App
+export default App;
