4e7bd013fbaf1c146c51e8dd9cfee1cd25060069
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-09-19 13:14:58 +0200

    Add: ConfigPanel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Configurator from "./components/Configurator/Configurator.jsx";
 import ConfiguratorProvider from "./context/ConfiguratorContext.jsx";
-import TextInput from "./components/TextInput/TextInput.jsx";
-import { colorPick, finishPick } from "./lib/configOptions.js";
-import RadioContainer from "./components/RadioContainer/RadioContainer.jsx";
+import ConfiguratorSection from "./components/ConfiguratorSection/ConfiguratorSection.jsx";
 function App() {
   return (
     <ConfiguratorProvider>
@@ -11,10 +8,7 @@ function App() {
         <Navbar />
       </header>
       <main>
-        <Configurator />
-        <TextInput label="Text" placeholder="Your text here..." />
-        <RadioContainer header="Color" name="color" options={colorPick} />
-        <RadioContainer header="Finish" name="finish" options={finishPick} />
+        <ConfiguratorSection />
       </main>
     </ConfiguratorProvider>
   );
