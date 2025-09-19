99cabef8ecf30437ebc9313cf9507501f4af7f7c
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-09-19 12:05:25 +0200

    radio button work

--- src/lib/configOptions.js
+++ src/lib/configOptions.js
@@ -1,13 +1,22 @@
 export const colorPick = {
-  0: "Blue",
-  1: "Pink",
-  2: "Red",
-  3: "Silver",
-  4: "Gold",
+  0: { label: "Blue", fillColor: "#446089" },
+  1: { label: "Pink", fillColor: "#F4B3FF" },
+  2: { label: "Red", fillColor: "#E17171" },
+  3: {
+    label: "Silver",
+    fillColor: "linear-gradient(to top, #6f6f6fff, #e0e0e0)",
+  },
+  4: {
+    label: "Gold",
+    fillColor: "linear-gradient(to top, #735407ff, #ffd700)",
+  },
 };
 
 export const finishPick = {
-  0: "Matte",
-  1: "Glossy",
-  2: "Holographic",
+  0: { label: "Matte", fillColor: "#EFEFEF" },
+  1: {
+    label: "Glossy",
+    fillColor: "linear-gradient(to right, #6f6f6fff, #e0e0e0)",
+  },
+  2: { label: "Holographic", fillColor: "#ba68c8" },
 };
