0f73157454ebcffa0c3fab9431299f74755bb8cd
Max Johansson <max.jv.johansson@gmail.com>
2025-09-19 13:19:15 +0200

    Add: PatternSelector component with state handling/styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
 import ConfiguratorProvider from "./context/ConfiguratorContext.jsx";
 import ConfiguratorSection from "./components/ConfiguratorSection/ConfiguratorSection.jsx";
+
 function App() {
   return (
     <ConfiguratorProvider>
