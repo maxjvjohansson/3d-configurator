88c5b9ca2d43c84cd3c5e5b7cdd87d6e687fa06c
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-09-19 09:52:55 +0200

    radio work

---
+++ src/lib/configOptions.js
@@ -0,0 +1,13 @@
+export const colorPick = {
+  0: "Blue",
+  1: "Pink",
+  2: "Red",
+  3: "Silver",
+  4: "Gold",
+};
+
+export const finishPick = {
+  0: "Matte",
+  1: "Glossy",
+  2: "Holographic",
+};
