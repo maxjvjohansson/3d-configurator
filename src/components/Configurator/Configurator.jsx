9d72c981b4233469aa7a41df306df30959e5023f
anna-dahlberg <anna.dahlberg@live.se>
2025-09-23 16:08:29 +0200

    more lighting adjustments

--- src/components/Configurator/Configurator.jsx
+++ src/components/Configurator/Configurator.jsx
@@ -18,9 +18,27 @@ function CreditCard() {
 
 	useEffect(() => {
 		if (materials['Rosa']) {
-			materials['Rosa'].color.set(colorMap[color])
+			const material = materials['Rosa']
+
+			material.color.set(colorMap[color])
+
+			if (finish === 'matte') {
+				material.metalness = 0
+				material.roughness = 1
+				material.envMapIntensity = 0
+			} else if (finish === 'shiny') {
+				material.metalness = 0.1
+				material.roughness = 0.3
+				material.envMapIntensity = 1
+			} else {
+				material.metalness = 0.8
+				material.roughness = 0.1
+				material.envMapIntensity = 0
+			}
+
+			material.needsUpdate = true
 		}
-	}, [color, materials, colorMap])
+	}, [color, finish, materials, colorMap])
 
 	return <primitive object={scene} scale={1.4} position={[0, -1.5, 0]} />
 }
@@ -29,8 +47,9 @@ export default function Configurator() {
 	return (
 		<section className="configurator-section">
 			<Canvas>
-				<ambientLight intensity={1} />
-				<directionalLight position={[3, 2, 5]} intensity={8} />
+				<Environment preset="warehouse" environmentIntensity={0.5} />
+				<directionalLight position={[-3, 2, -5]} intensity={4} />
+				<directionalLight position={[2, 1, 2]} intensity={3} />
 				<CreditCard />
 				<OrbitControls />
 			</Canvas>
