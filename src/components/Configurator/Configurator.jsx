5260c82ba43644fdef1d5b7dabb39fc9c57bad9a
anna-dahlberg <anna.dahlberg@live.se>
2025-09-23 11:25:50 +0200

    removed lighting to start fresh

--- src/components/Configurator/Configurator.jsx
+++ src/components/Configurator/Configurator.jsx
@@ -16,49 +16,15 @@ function CreditCard() {
 		gold: '#CCA149'
 	}
 
-	useEffect(() => {
-		if (materials['Rosa']) {
-			const originalMaterial = materials['Rosa']
-			let workingMaterial = originalMaterial.clone()
-
-			workingMaterial.color.set(colorMap[color])
-
-			if (finish === 'matte') {
-				workingMaterial.metalness = 0
-				workingMaterial.roughness = 1
-			} else if (finish === 'shiny') {
-				workingMaterial.metalness = 0
-				workingMaterial.roughness = 0.1
-			} else if (finish === 'metallic') {
-				workingMaterial.metalness = 1
-				workingMaterial.roughness = 0.2
-			}
-
-			if (nodes['KORT_front']) {
-				nodes['KORT_front'].material = workingMaterial
-			}
-		}
-	}, [color, finish, materials, nodes, colorMap])
-
 	return <primitive object={scene} scale={1.4} position={[0, -1.5, 0]} />
 }
 
 export default function Configurator() {
-	const { finish } = useConfigurator()
-
 	return (
 		<section className="configurator-section">
 			<Canvas>
-				{finish === 'metallic' && (
-					<Environment
-						preset="studio"
-						background={false}
-						environmentIntensity={0.3}
-					/>
-				)}
-				<ambientLight intensity={1.5} />
-				<directionalLight position={[3, 5, 8]} intensity={2} />
-				<directionalLight position={[-3, 3, 5]} intensity={0.8} />
+				<ambientLight intensity={1} />
+				<directionalLight position={[5, 5, 5]} intensity={1} />
 				<CreditCard />
 				<OrbitControls />
 			</Canvas>
