b158aec398bb0a2edfce0a10743eaa4da7ffc263
anna-dahlberg <anna.dahlberg@live.se>
2025-09-24 11:51:39 +0200

    adjusted code for pattern names and spelling error in new glb files

--- src/components/Configurator/Configurator.jsx
+++ src/components/Configurator/Configurator.jsx
@@ -6,9 +6,9 @@ import { useConfigurator } from '../../context/ConfiguratorContext'
 
 function CreditCard() {
 	const { pattern, color, finish, text } = useConfigurator()
-	const { scene, materials, nodes } = useGLTF(
-		`../../../credit_card/${pattern}.gltf`
-	)
+
+	const modelPath = `../../../credit_card/${pattern || 'none'}.glb`
+	const { scene, materials, nodes } = useGLTF(modelPath)
 
 	const colorMap = {
 		blue: '#31417D',
@@ -19,8 +19,12 @@ function CreditCard() {
 	}
 
 	useEffect(() => {
-		if (materials['Rosa']) {
-			const material = materials['Rosa']
+		const materialName = Object.keys(materials).find(
+			(name) => name.startsWith('Grund') || name.startsWith('Grud') //Adjusting for spelling error in glb file
+		)
+
+		if (materials[materialName]) {
+			const material = materials[materialName]
 
 			material.color.set(colorMap[color])
 
@@ -39,8 +43,13 @@ function CreditCard() {
 			}
 
 			material.needsUpdate = true
+		} else {
+			console.warn(
+				'Main card material not found. Available:',
+				Object.keys(materials)
+			)
 		}
-	}, [color, finish, materials])
+	}, [color, finish, materials]) // Removed colorMap from dependencies
 
 	return (
 		<group scale={1.4} position={[0, -1.5, 0]}>
@@ -63,11 +72,11 @@ export default function Configurator() {
 	return (
 		<section className="configurator-section">
 			<Canvas>
-				<Environment preset="warehouse" environmentIntensity={0.8} />
+				<Environment preset="warehouse" environmentIntensity={0.5} />
 				<directionalLight position={[-3, 2, -5]} intensity={4} />
 				<directionalLight position={[2, 1, 2]} intensity={3} />
 				<CreditCard />
-				<OrbitControls minDistance={2} maxDistance={6} />
+				<OrbitControls minDistance={3} maxDistance={15} />
 			</Canvas>
 		</section>
 	)
