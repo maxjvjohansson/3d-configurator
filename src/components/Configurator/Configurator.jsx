6b594983ccf37df26b67103e6fe3b33d0a590fbf
anna-dahlberg <anna.dahlberg@live.se>
2025-09-25 13:00:30 +0200

    configured lighting settings based on finsish

--- src/components/Configurator/Configurator.jsx
+++ src/components/Configurator/Configurator.jsx
@@ -7,9 +7,6 @@ import { useConfigurator } from '../../context/ConfiguratorContext'
 function CreditCard() {
 	const { pattern, color, finish, text } = useConfigurator()
 	const modelPath = `/credit_card/${pattern}/${pattern}_${color}/${pattern}_${color}.gltf`
-
-	console.log('Loading model:', modelPath)
-
 	const { scene, materials } = useGLTF(modelPath)
 
 	useEffect(() => {
@@ -52,12 +49,33 @@ function CreditCard() {
 }
 
 export default function Configurator() {
+	const { finish } = useConfigurator()
+
+	let environmentIntensity, lightIntensity1, lightIntensity2
+
+	if (finish === 'matte') {
+		environmentIntensity = 0.3
+		lightIntensity1 = 1
+		lightIntensity2 = 1
+	} else if (finish === 'glossy') {
+		environmentIntensity = 0.4
+		lightIntensity1 = 3
+		lightIntensity2 = 3
+	} else if (finish === 'metallic') {
+		environmentIntensity = 0.8
+		lightIntensity1 = 4
+		lightIntensity2 = 4
+	}
+
 	return (
 		<section className="configurator-section">
 			<Canvas>
-				<Environment preset="warehouse" environmentIntensity={0.5} />
-				<directionalLight position={[-3, 2, -5]} intensity={4} />
-				<directionalLight position={[2, 1, 2]} intensity={3} />
+				<Environment
+					preset="warehouse"
+					environmentIntensity={environmentIntensity}
+				/>
+				<directionalLight position={[-3, 2, -5]} intensity={lightIntensity1} />
+				<directionalLight position={[2, 1, 2]} intensity={lightIntensity2} />
 				<CreditCard />
 				<OrbitControls minDistance={3} maxDistance={15} />
 			</Canvas>
